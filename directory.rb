cfb393903806c7a7f3b9cc0d3a4530c913fd7033
Laura Cabantous <laura.cabantous@gmail.com>
2022-03-04 16:05:26 +0000

    Refactored code

--- directory.rb
+++ directory.rb
@@ -1,11 +1,10 @@
 # Let's put all students into an array
-
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
 
     # Create an empty array
-    students =[]
+    @students =[]
 
     #Get the name
     name = gets.chomp
@@ -14,15 +13,15 @@ def input_students
 
     while !name.empty? do
         # Add the student hash to the array
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
 
         # Get another name from the user
         name = gets.chomp
     end
 
     # Return the array of students
-    students
+    @students
 end
 
 def print_header
@@ -31,16 +30,47 @@ def print_header
 end
 
 def print(students)
-    students.each_with_index do | student, index |
-        puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    @students.each do | student |
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+    print_header
+    print(@students)
+    print_footer(@students)
+end
+
+def process(selection)
+    case selection
+         when "1"
+            input_students
+        when "2"
+            show_students
+        when "9"
+            exit # this will cause the program to terminate
+        else
+            puts "I don't know what you meant, try again"
+    end
+end
+
+
+
+def interactive_menu
+    loop do
+        print_menu
+        process(gets.chomp)
+    end
+end
+
+interactive_menu
