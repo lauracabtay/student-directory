90ff0b4c0697878163788a396ba8bb5256dc695a
Laura Cabantous <laura.cabantous@gmail.com>
2022-03-04 17:39:30 +0000

    Added method to load file on program start

--- directory.rb
+++ directory.rb
@@ -1,10 +1,10 @@
+# Create an empty array
+@students =[]
+
 # Let's put all students into an array
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-
-    # Create an empty array
-    @students =[]
 
     #Get the name
     name = gets.chomp
@@ -17,7 +17,7 @@ def input_students
         puts "Now we have #{@students.count} students"
 
         # Get another name from the user
-        name = gets.chomp
+        name = STDIN.gets.chomp
     end
 
     # Return the array of students
@@ -43,6 +43,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit"
 end
 
@@ -60,6 +61,8 @@ def process(selection)
             show_students
         when "3"
             save_students
+        when "4"
+            load_students
         when "9"
             exit # this will cause the program to terminate
         else
@@ -70,7 +73,7 @@ end
 def interactive_menu
     loop do
         print_menu
-        process(gets.chomp)
+        process(STDIN.gets.chomp)
     end
 end
 
@@ -86,4 +89,26 @@ def save_students
     file.close
 end
 
+def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
+    file.readlines.each do | line |
+        name, cohort = line.chomp.split(",")
+        @students << {:name => name, :cohort => cohort.to_sym}
+    end
+    file.close
+end
+
+def try_load_students
+    filename = ARGV.first
+    return if filename.nil?
+    if File.exists?(filename)
+        load_students(filename)
+        puts "Loaded #{@students.count} from #{filename}"
+    else
+        puts "Sorry, #{filename} doesn't exist."
+        exit
+    end
+end
+
+try_load_students
 interactive_menu
